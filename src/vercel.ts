2c8bd725e9eda063945e8b814d6877ebf30bc4d1
Dimitri Sime <unkutnation@gmail.com>
2026-06-07 05:49:28 -0400

    feat: update budget limits in angular.json and add Vercel configuration

---
+++ src/vercel.ts
@@ -0,0 +1,22 @@
+﻿import type { VercelConfig } from '@vercel/config/v1';
+
+//my-clinic-trinity-fe
+export const config: VercelConfig = {
+  git: {
+    deploymentEnabled: {
+      'my-clinic-*': false,
+      '*-fe': true,
+    },
+  },
+};
+
+/*import type { VercelConfig } from '@vercel/config/v1';
+ A branch named experiment-my-branch-dev will create a deployment.
+export const config: VercelConfig = {
+  git: {
+    deploymentEnabled: {
+      'experiment-*': false,
+      '*-dev': true,
+    },
+  },
+};*/
