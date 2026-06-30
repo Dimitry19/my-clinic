0b8d7d4e935033c107df07e5e95de339c4d26319
Dimitri Sime <unkutnation@gmail.com>
2026-06-30 16:03:11 -0400

    Gestion de la facturation.

---
+++ facturation/src/test/java/it/solutions/services/trinity/facturation/FacturationApplicationTests.java
@@ -0,0 +1,13 @@
+package it.solutions.services.trinity.facturation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacturationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
